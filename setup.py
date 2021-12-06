5a12949da67f2591bf292e82bfc0fdf69dbbfe08
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2021-12-06 14:30:42 -0500

    Added MAPoverty Scale, AL+MassAccess to requires

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name='docassemble.ALAffidavitOfIndigency',
       url='https://docassemble.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=[],
+      install_requires=['docassemble.AssemblyLine', 'docassemble.MassAccess', 'docassemble.MAPovertyScale'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/ALAffidavitOfIndigency/', package='docassemble.ALAffidavitOfIndigency'),
      )
