ff1b2343b354d23e6509486ae7fe97c1fdcd0db6
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2022-09-29 15:11:19 -0400

    Install PovertyScale, not MAPovertyScale

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name='docassemble.ALAffidavitOfIndigency',
       url='https://docassemble.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['docassemble.AssemblyLine', 'docassemble.MassAccess', 'docassemble.MAPovertyScale', 'docassemble.income', 'docassemble.ALMassachusetts>=0.1.2'],
+      install_requires=['docassemble.AssemblyLine', 'docassemble.MassAccess', 'docassemble.PovertyScale', 'docassemble.income', 'docassemble.ALMassachusetts>=0.1.2'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/ALAffidavitOfIndigency/', package='docassemble.ALAffidavitOfIndigency'),
      )
